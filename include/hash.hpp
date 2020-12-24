6efdb94bf7d62de706d42dea163d739169b1892e
Wh1teT1ger <burylov01@mail.ru>
2020-12-24 23:27:27 +0500

    add files

---
+++ include/hash.hpp
@@ -0,0 +1,86 @@
+// Copyright 2020 Burylov Denis <burylov01@mail.ru>
+
+#ifndef LAB_06_MULTITHREADS_HASH_HPP
+#define LAB_06_MULTITHREADS_HASH_HPP
+
+#include <picosha2.h>
+
+#include <boost/log/trivial.hpp>
+#include <boost/thread/mutex.hpp>
+#include <ctime>
+#include <nlohmann/json.hpp>
+#include <random>
+#include <string>
+#include <thread>
+#include <vector>
+
+using namespace nlohmann;
+std::mutex mut;
+
+class Hash {
+  std::string data_, hash_;
+  long time_;
+
+ public:
+  Hash(const std::string& data)
+      : data_(data), hash_(picosha2::hash256_hex_string(data)), time_(0) {}
+
+  bool check() { return hash_.substr(hash_.size() - 4, 4) == "0000"; }
+
+  void set_time(const long& time) { time_ = time; }
+
+  const std::string& data() { return data_; }
+
+  const std::string& hash() { return hash_; }
+
+  const long& time() { return time_; }
+};
+std::vector<Hash> hash_array;
+
+void find_hash(std::atomic_bool* shutdown) {
+  srand(time(NULL));
+  auto begin = std::chrono::steady_clock::now();
+  for (;;) {
+    Hash hash{std::to_string(rand())};
+    if (hash.check()) {
+      BOOST_LOG_TRIVIAL(info)
+          << "Thread id: " << std::this_thread::get_id()
+          << " Data: " << hash.data() << " Hash: " << hash.hash();
+      auto end = std::chrono::steady_clock::now();
+      mut.lock();
+      hash.set_time(
+          std::chrono::duration_cast<std::chrono::milliseconds>(end - begin)
+              .count());
+      hash_array.push_back(hash);
+      mut.unlock();
+      begin = std::chrono::steady_clock::now();
+      // break;
+    } else {
+      BOOST_LOG_TRIVIAL(trace)
+          << "Thread id: " << std::this_thread::get_id()
+          << " Data: " << hash.data() << " Hash: " << hash.hash();
+    }
+    if (*shutdown) {
+      break;
+    }
+  }
+}
+
+json to_json_hash(const std::vector<Hash>& vec) {
+  json vector = json::array();
+  for (auto hash : vec) {
+    json j;
+    j["timestamp"] = hash.time();
+    j["hash"] = hash.hash();
+    j["data"] = hash.data();
+    vector.push_back(j);
+  }
+  return vector;
+}
+
+void write_to_file(std::ofstream& out, const std::vector<Hash>& vec) {
+  json j = to_json_hash(vec);
+  out << std::setw(4) << j;
+}
+
+#endif  // LAB_06_MULTITHREADS_HASH_HPP
---
+++ sources/main.cpp
@@ -0,0 +1,95 @@
+// Copyright 2020 Burylov Denis <burylov01@mail.ru>
+
+#include <boost/log/keywords/file_name.hpp>
+#include <boost/log/keywords/format.hpp>
+#include <boost/log/keywords/rotation_size.hpp>
+#include <boost/log/keywords/time_based_rotation.hpp>
+#include <boost/log/sinks.hpp>
+#include <boost/log/utility/setup/console.hpp>
+#include <csignal>
+#include <hash.hpp>
+
+namespace logging = boost::log;
+namespace sinks = logging::sinks;
+namespace keywords = logging::keywords;
+namespace expressions = logging::expressions;
+
+void init() {
+  boost::shared_ptr<logging::core> core = logging::core::get();
+  // logs to file for info
+  boost::shared_ptr<sinks::text_file_backend> backend1 =
+      boost::make_shared<sinks::text_file_backend>(
+          keywords::file_name = "logs/file_info_%5N.log",
+          keywords::rotation_size = 5 * 1024 * 1024,
+          keywords::format = "[%TimeStamp%]: %Message%",
+          keywords::time_based_rotation =
+              sinks::file::rotation_at_time_point(12, 0, 0));
+
+  typedef sinks::synchronous_sink<sinks::text_file_backend> sink_file;
+  boost::shared_ptr<sink_file> sink1(new sink_file(backend1));
+  sink1->set_filter(logging::trivial::severity >= logging::trivial::info);
+  core->add_sink(sink1);
+
+  // logs to file for trace
+  boost::shared_ptr<sinks::text_file_backend> backend2 =
+      boost::make_shared<sinks::text_file_backend>(
+          keywords::file_name = "logs/file_trace_%5N.log",
+          keywords::rotation_size = 5 * 1024 * 1024,
+          keywords::format = "[%TimeStamp%]: %Message%",
+          keywords::time_based_rotation =
+              sinks::file::rotation_at_time_point(12, 0, 0));
+
+  boost::shared_ptr<sink_file> sink2(new sink_file(backend2));
+  sink2->set_filter(logging::trivial::severity <= logging::trivial::trace);
+  core->add_sink(sink2);
+
+  // logs to console
+  boost::shared_ptr<sinks::text_ostream_backend> backend3 =
+      boost::make_shared<sinks::text_ostream_backend>();
+  backend3->add_stream(
+      boost::shared_ptr<std::ostream>(&std::clog, boost::null_deleter()));
+
+  typedef sinks::synchronous_sink<sinks::text_ostream_backend> sink_console;
+  boost::shared_ptr<sink_console> sink3(new sink_console(backend3));
+  sink3->set_filter(logging::trivial::severity >= logging::trivial::info);
+  core->add_sink(sink3);
+}
+
+int main(int argc, char* argv[]) {
+  init();
+  auto threads_count = boost::thread::hardware_concurrency();
+  std::string file;
+  if (argc >= 2) {
+    if (std::stoul(argv[1]) > 0) threads_count = std::stoul(argv[1]);
+    if (argc == 3) file = argv[2];
+  }
+  BOOST_LOG_TRIVIAL(info) << threads_count << " threads";
+  BOOST_LOG_TRIVIAL(info) << "File: " << file;
+
+  static ::std::atomic_bool shutdown = false;
+  {
+    sig_t handler = [](int const signal) {
+      BOOST_LOG_TRIVIAL(info)
+          << "Shutting down due to signal " << signal << std::endl;
+      shutdown = true;
+    };
+    bsd_signal(SIGINT, handler);
+    bsd_signal(SIGSTOP, handler);
+    bsd_signal(SIGTERM, handler);
+  }
+  auto* p_shutdown = &shutdown;
+
+  std::vector<boost::thread> threads;
+  for (size_t i = 0; i < threads_count; i++) {
+    threads.emplace_back(boost::thread{find_hash, p_shutdown});
+  }
+  for (size_t i = 0; i < threads_count; i++) {
+    threads[i].join();
+  }
+  if (!file.empty()) {
+    std::ofstream out(file);
+    write_to_file(out, hash_array);
+    out.close();
+  }
+  return 0;
+}
